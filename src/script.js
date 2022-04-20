7eee488bd219e789a136ae3aa1f8510037c7f044
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-04-20 17:22:11 -0300

    all users quizzes are now showed. When these quizzes are clicked, the page goes to the questions (need to be do)

--- src/script.js
+++ src/script.js
@@ -8,11 +8,6 @@ function usuarioTemQuiz () {
     return false;
 }
 
-function listarQuizes(div) {
-    // função incompleta
-    div.querySelector(".quiz-cards");
-}
-
 function mostrarTelaInicial() {
     conteudoMutavel.innerHTML = `
     <div class="quizes">
@@ -54,5 +49,38 @@ function mostrarTelaCriacaoQuiz () {
     // Essa função está incompleta, precisamos poder criar quizz para completá-la
 }
 
+
+function listarQuizes() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
+    promise.then(putQuizes);
+}
+
+function putQuizes(quiz) {
+    let quizDosOutros = document.querySelector(".quiz-cards");
+    quizDosOutros.innerHTML = ""
+    for (let i = 0; i < quiz.data.length; i++) {
+        const htmlQuizz = `
+        <div class="quiz-card" onclick="showQuiz()">
+            <p>
+            ${quiz.data[i].title}
+            </p>
+        </div>
+        `
+        console.log(quiz.data[i].image)
+        quizDosOutros.innerHTML += htmlQuizz
+    }
+    // put the background image
+    let Allquizes = document.querySelectorAll(".quiz-card")
+    for (let i = 0; i < Allquizes.length; i++) {
+        Allquizes[i].style.backgroundImage = `${backgroundGradient}, url("${quiz.data[i].image}")`
+    }
+}
+
+function showQuiz() {
+    document.querySelector(".quizes").classList.add("none")
+    // add the quizz information
+}
+
 const conteudoMutavel = document.querySelector(".container");
+const backgroundGradient = "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%)"
 mostrarTelaInicial();
