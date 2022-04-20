2ec2677e959bd7ab9c970e2315e944aa35bd05e0
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-04-20 18:35:13 -0300

    Perguntas do quiz WIP

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 function usuarioTemQuiz () {
     // Essa função está incompleta, precisamos poder criar quizz para completá-la
-    const temQuiz = false;
+    const temQuiz = true;
     if (temQuiz) {
         return true;
     }
@@ -50,9 +50,93 @@ function mostrarBotaoPequeno (quizesDoUsuario) {
 }
 
 function mostrarTelaCriacaoQuiz () {
-    conteudoMutavel.innerHTML = "";
+    conteudoMutavel.innerHTML = `
+    <input type="button" value="Prosseguir pra criar perguntas" onclick="mostrarTelaCriacaoPerguntas()">`;
     // Essa função está incompleta, precisamos poder criar quizz para completá-la
 }
 
+function mostrarTelaCriacaoPerguntas() {
+    const inicio = `
+    <div class="criacao-perguntas">
+        <h2>Crie suas perguntas</h2>`;
+
+    let meio = ``;
+
+    const fim = `
+        <input type="button" value="Prosseguir pra criar níveis" class="prosseguir" onclick="checkContent()"/>
+    </div>`;
+
+    for (let i = 0; i < numeroPerguntas; i++) {
+        meio += `
+        <div class="pergunta">
+            <h3>Pergunta ${i + 1} <ion-icon name="create-outline" onclick="mostra(this)"></ion-icon></h3>
+            <div class="wrapper hidden">
+                <input type="text" placeholder="Texto da pergunta" name="pergunta"/>
+                <input type="text" placeholder="Cor de fundo da pergunta" name="cor-da-pergunta"/>
+                <div class="sep"></div>
+                <h3>Resposta correta</h3>
+                <input type="text" placeholder="Resposta correta" name="resposta"/>
+                <input type="text" placeholder="URL da imagem" name="url-resposta"/>
+                <div class="sep"></div>
+                <h3>Respostas incorretas</h3>
+                <input type="text" placeholder="Resposta incorreta 1" name="resposta-errada1"/>
+                <input type="text" placeholder="URL da imagem 1" name="url-resposta-errada1"/>
+                <div class="sep"></div>
+                <input type="text" placeholder="Resposta incorreta 2" name="resposta-errada2"/>
+                <input type="text" placeholder="URL da imagem 2" name="url-resposta-errada2"/>
+                <div class="sep"></div>
+                <input type="text" placeholder="Resposta incorreta 3" name="resposta-errada3"/>
+                <input type="text" placeholder="URL da imagem 3" name="url-resposta-errada3"/>
+            </div>
+        </div>`;
+    }
+
+    conteudoMutavel.innerHTML = inicio + meio + fim;
+
+    mostra(document.querySelector(".pergunta ion-icon"));
+}
+
+function mostra(icone) {
+    if (mostrando !== undefined) {
+        esconde(mostrando);
+    }
+    icone.classList.add("hidden");
+    const pergunta = icone.parentNode.parentNode
+    pergunta.querySelector(".wrapper").classList.remove("hidden");
+    mostrando = pergunta;
+}
+
+function esconde(pergunta) {
+    pergunta.querySelector("h3 ion-icon").classList.remove("hidden");
+    pergunta.querySelector(".wrapper").classList.add("hidden");
+}
+
+function checkContent() {
+    const perguntas = document.querySelectorAll(".pergunta");
+    
+    perguntas.map(pergunta => {
+        const inputs = pergunta.querySelectorAll("input");
+        inputs.map(input => {
+            switch (input.name) {
+                case "pergunta":
+                    if (input.value.length < 20) {
+                        alert("Pergunta deve ter mais de 20 caracteres.")
+                    }
+                    break;
+                case "cor-da-pergunta":
+                    const hex = /#[0-9A-Fa-f]{6}/g;
+                    if (!hex.test(input.value)) {
+                        alert("Cor de fundo deve ser uma cor em hexadecimal válida");
+                    }
+                    break;
+            }
+        })
+    })
+}
+
 const conteudoMutavel = document.querySelector(".container");
+let mostrando;
+// mock para numero de perguntas
+let numeroPerguntas = 5;
+
 mostrarTelaInicial();
