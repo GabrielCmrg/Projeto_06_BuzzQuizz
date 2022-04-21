f871793ee5aaadb244185dfdc2933f5c057344cb
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-04-21 15:41:06 -0300

    back to normal

--- src/script.js
+++ src/script.js
@@ -423,5 +423,4 @@ const conteudoMutavel = document.querySelector(".container");
 let mostrando;
 let basicInfos = {quizTitle: "", quizImageSrc: "", numberOfQuestions: 1, numberOfLevels: 0};
 const backgroundGradient = "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%)";
-//mostrarTelaInicial();
-mostrarTelaCriacaoPerguntas();
+mostrarTelaInicial();
