43f8adac5492a025e6ba84ce00cd7c029b3f3878
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-04-22 19:50:24 -0300

    merged files

--- src/script.js
+++ src/script.js
@@ -683,5 +683,4 @@ const levels = [];
 let infoQuizzes = [];
 const backgroundGradient = "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%)";
 mostrarTelaInicial();
-// enviarquizServer();
  
